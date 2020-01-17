39f43b127257d87d2731146739288680b6e6f23e
Emmanuel Cruz <iphoneauta4g@gmail.com>
2020-01-17 15:44:28 -0600

    Commit :  Dos
    Dos

---
+++ app/src/main/java/com/arggos/merecomiendas/Otros_Servicios.java
@@ -0,0 +1,52 @@
+package com.arggos.merecomiendas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Otros_Servicios extends AppCompatActivity {
+
+    RecyclerView rv;
+    List<Profesion> prof;
+    Servicios serv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otros__servicios);
+
+        rv = findViewById(R.id.Res1);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        prof = new ArrayList<>();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        serv = new Servicios(prof);
+        rv.setAdapter(serv);
+        database.getReference().getRoot().addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                prof.removeAll(prof);
+                for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+                    Profesion profe = new Profesion(snapshot.getValue(String.class));
+                    prof.add(profe);
+                }
+                serv.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/arggos/merecomiendas/Profesion.java
@@ -0,0 +1,21 @@
+package com.arggos.merecomiendas;
+
+public class Profesion {
+
+    String nombre;
+
+    public Profesion(){
+
+    }
+    public Profesion(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
